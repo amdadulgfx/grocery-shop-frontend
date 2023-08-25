69edfc8d59bf28a78336fa91aed399d36aa1b1a6
Md Amdadul Haq Arif <arifweb24@gmail.com>
2023-08-25 08:20:22 +0600

    landing page conflict resolved

---
+++ src/pages/LandingPage/CategoriesBottom.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+
+const Categories = () => {
+  const categories = [
+    {
+      id: 1,
+      category: "Beverages",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/baverages-1.jpg",
+      totalItem: 11,
+    },
+    {
+      id: 2,
+      category: "Biscuits & Snacks",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/biscuitssnacks-1.jpg",
+      totalItem: 6,
+    },
+    {
+      id: 3,
+      category: "Breads & Bakery",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/breadbakery-1.jpg",
+      totalItem: 6,
+    },
+    {
+      id: 4,
+      category: "Breakfast & Dairy",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/dairy-1.jpg",
+      totalItem: 8,
+    },
+    {
+      id: 5,
+      category: "Frozen Foods",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/04/category-image4.png",
+      totalItem: 8,
+    },
+    {
+      id: 6,
+      category: "Fruits & Vegetables",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/fruitvegetables-1.jpg",
+      totalItem: 12,
+    },
+    {
+      id: 7,
+      category: "Grocery & Staples",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/04/category-image2.png",
+      totalItem: 12,
+    },
+    {
+      id: 8,
+      category: "Household Needs",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/household-1.jpg",
+      totalItem: 1,
+    },
+    {
+      id: 9,
+      category: "Meats & Seafood",
+      image:
+        "https://klbtheme.com/bacola/wp-content/uploads/2021/05/meat-1.jpg",
+      totalItem: 5,
+    },
+  ];
+  const [beverage, ...otherCategories] = categories;
+
+  const style = {
+    regularCategories: {
+      padding: "1.25rem",
+      border: ["none", "1px solid #e4e5ee"],
+      textAlign: "center",
+      display: "flex",
+      width: ["50%", "33.33%", "25%"],
+      boxSizing: "border-box",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          border: "1px solid #e4e5ee",
+          display: "flex",
+          flexDirection: ["column", "row"],
+          margin: ["1rem", "3rem", "5rem"],
+        }}
+      >
+        <Box
+          sx={{
+            border: "1px solid #e4e5ee",
+            textAlign: "center",
+            width: ["100%", "30%"],
+          }}
+        >
+          <Box padding="1.25rem">
+            <img
+              style={{
+                height: "12rem",
+                width: "12rem",
+              }}
+              src={beverage.image}
+              alt=""
+            />
+
+            <Typography
+              sx={{
+                fontSize: ".875rem",
+                fontWeight: "600",
+              }}
+            >
+              {beverage.category}
+            </Typography>
+            <Typography variant="caption" display="block" color="#a18f96">
+              {beverage.totalItem} Items
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            width: ["100%", "70%"],
+          }}
+        >
+          {otherCategories?.map((category) => (
+            <Box key={category.id} sx={style.regularCategories}>
+              <img height="70" width="70" src={category?.image} alt="" />
+              <Box>
+                <Typography
+                  sx={{
+                    fontSize: ".875rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  {category?.category}
+                </Typography>
+                <Typography variant="caption" display="block" color="#a18f96">
+                  {category?.totalItem} Items
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Categories;
--- src/pages/LandingPage/LandingPage.view.jsx
+++ src/pages/LandingPage/LandingPage.view.jsx
@@ -1,74 +1,67 @@
 import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
-import React, { useState, useEffect, } from "react";
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import React, { useState, useEffect } from "react";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import Categories from "./CategoriesBottom";
 
 const titles = {
-    subTitle: "Exclusive offer",
-    offer: "-20% off",
-    heading: "Specialist in the grocery store",
-    message: "Only this week. Don't miss...",
-    price: "$7.99",
-    route: "/shop"
-}
+  subTitle: "Exclusive offer",
+  offer: "-20% off",
+  heading: "Specialist in the grocery store",
+  message: "Only this week. Don't miss...",
+  price: "$7.99",
+  route: "/shop",
+};
 
 const LandingPage = () => {
-    const [heroMasterData, setHeroMasterData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get("http://localhost:5000/api/v1/heroMasterData");
-                setHeroMasterData(response.data);
-            } catch (err) {
-                console.log(err);
-            } finally {
-                setLoading(false);
-            }
-        }
+  const [heroMasterData, setHeroMasterData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/heroMasterData"
+        );
+        setHeroMasterData(response.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    console.log(heroMasterData)
+  console.log(heroMasterData);
 
-    return (
-        <Box maxWidth="md" sx={{mx: "auto", my: 5}}>
-            <Box>
-                <Typography
-                    variant="subtitle2"
-                >
-                    {titles.subTitle.toUpperCase()}
-                    <span>{titles.offer.toUpperCase()}</span>
-                </Typography>
-                <Typography
-                    variant="h3"
-                >
-                    {titles.heading}
-                </Typography>
-                <Typography
-                    variant="subtitle1"
-                >
-                    {titles.message}
-                </Typography>
-                <Typography
-                    variant="h5"
-                >
-                    from {titles.price}
-                </Typography>
-                <Button
-                    variant="contained"
-                    sx={{
-                        textTransform: 'none', 
-                        borderRadius: 16,
-                        px: 3,
-                      }}
-                >
-                    Shop Now &nbsp;<ArrowRightAltIcon />
-                </Button>
-            </Box>
+  return (
+    <>
+      <Box maxWidth="md" sx={{ mx: "auto", my: 5 }}>
+        <Box>
+          <Typography variant="subtitle2">
+            {titles.subTitle.toUpperCase()}
+            <span>{titles.offer.toUpperCase()}</span>
+          </Typography>
+          <Typography variant="h3">{titles.heading}</Typography>
+          <Typography variant="subtitle1">{titles.message}</Typography>
+          <Typography variant="h5">from {titles.price}</Typography>
+          <Button
+            variant="contained"
+            sx={{
+              textTransform: "none",
+              borderRadius: 16,
+              px: 3,
+            }}
+          >
+            Shop Now &nbsp;
+            <ArrowRightAltIcon />
+          </Button>
         </Box>
-    );
+      </Box>
+      <Categories />
+    </>
+  );
 };
 
 export default LandingPage;
