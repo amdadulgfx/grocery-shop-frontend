fd393493f41b3daa2c655d62007f89e97dd85bf9
Yeasaleh <saleh.y@medlinkjobs.com>
2023-09-10 19:35:44 +0600

    git pull

--- src/pages/AdminDashboard/AdminDashboard.view.jsx
+++ src/pages/AdminDashboard/AdminDashboard.view.jsx
@@ -1,22 +1,10 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+// import { Outlet } from 'react-router-dom';
+import { AppBar, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
 
 const drawerWidth = 240;
 const AdminDashboard = (props) => {
@@ -31,7 +19,6 @@ const AdminDashboard = (props) => {
       <div>
         <Toolbar />
         <Divider />
-        <Link to={`/post-product`} >Add Product</Link>
         <List>
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem key={text} disablePadding>
