3cf903e5f6a06aae55885a4ab23a4ef084c3aefb
Md Amdadul Haq Arif <arifweb24@gmail.com>
2023-09-12 07:56:00 +0600

    showing reviews on single product

--- src/pages/SingleProduct/Components/AdditionalDetailsTab.jsx
+++ src/pages/SingleProduct/Components/AdditionalDetailsTab.jsx
@@ -4,6 +4,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Review from './Review';
 
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -73,7 +74,7 @@ export default function AdditionalDetailsTab() {
                 Morbi ut sapien vitae odio accumsan gravida. Morbi vitae erat auctor, eleifend nunc a, lobortis neque. Praesent aliquam dignissim viverra. Maecenas lacus odio, feugiat eu nunc sit amet, maximus sagittis dolor. Vivamus nisi sapien, elementum sit amet eros sit amet, ultricies cursus ipsum. Sed consequat luctus ligula. Curabitur laoreet rhoncus blandit. Aenean vel diam ut arcu pharetra dignissim ut sed leo. Vivamus faucibus, ipsum in vestibulum vulputate, lorem orci convallis quam, sit amet consequat nulla felis pharetra lacus. Duis semper erat mauris, sed egestas purus commodo vel.
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
-                Coming Soon...
+                <Review />
             </CustomTabPanel>
         </Box>
     );
---
+++ src/pages/SingleProduct/Components/Review.jsx
@@ -0,0 +1,47 @@
+import { Box, Rating, Typography } from '@mui/material'
+import axios from 'axios'
+import { DateTime } from 'luxon'
+import React, { useEffect, useState } from 'react'
+
+function Review() {
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URI}review/64f2d42ab495ff1e017068d2`).then(res => {
+            console.log(res);
+            if (res.data.success) {
+                setReviews(res.data.data)
+            }
+        })
+    }, [])
+    return (
+        <>
+            {
+                reviews.map(rev =>
+                    <Box sx={{ display: "flex", mb: '.5rem' }}>
+                        <Box component="img" src='https://secure.gravatar.com/avatar/dd28514c9a8cfba334e05f21703be28e?s=60&d=mm&r=g' sx={{ borderRadius: "2rem", height: "100%", mr: '1rem' }} />
+                        <Box>
+                            <Rating
+                                sx={{ fontSize: '1rem', mt: 0.6 }}
+                                readOnly
+                                value={rev?.rating}
+                            />
+                            <Typography sx={{ fontWeight: "600", fontSize: "0.95rem", }}>
+                                {rev?.userId?.name?.firstName}
+                                <span
+                                    style={{ color: "lightgray", fontSize: "0.825rem", fontWeight: "400", marginLeft: '0.5rem' }}>
+                                    - {DateTime.fromISO(rev?.createdAt).toLocaleString(DateTime.DATE_MED)}
+                                </span>
+                            </Typography>
+                            <Typography sx={{ fontSize: "0.8rem", color: "#424542" }} >
+                                {rev?.description}
+                            </Typography>
+                        </Box>
+                    </Box>
+                )
+            }
+
+        </>
+    )
+}
+
+export default Review
