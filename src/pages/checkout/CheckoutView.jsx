dc5c981300fa22815ac705ab540f4947b8a8d8ae
Kamruzzaman <zaman.rabeen@gmail.com>
2023-10-17 22:01:30 +0600

    checkout pages desing

---
+++ src/pages/checkout/CheckoutView.jsx
@@ -0,0 +1,180 @@
+import { Box, Checkbox, Container, Divider, FormControlLabel, Grid, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import { purple } from '@mui/material/colors';
+import { styled } from '@mui/material/styles';
+import axios from 'axios';
+import { useFormik } from 'formik';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import { BillingAdress } from './component/BillingAdress';
+const SubmitButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: '#ed174a',
+    '&:hover': {
+        backgroundColor: '#ed174a',
+    },
+    padding: '13px 8px'
+}));
+
+const billingDetails = {
+    email: "",
+    name: {
+        firstName: '',
+        lastName: '',
+    },
+    companyName: "",
+    streetAddress: "",
+    city: "",
+    zipCode: "",
+    phone: "",
+    orderNotes: ""
+}
+const validationSchema = yup.object({
+    name: yup.object().shape({
+        firstName: yup.string().required('Required'),
+        lastName: yup.string().required('Required'),
+    }),
+    email: yup.string().email('Invalid email').required('Required'),
+    streetAddress: yup.string().required('Required'),
+    city: yup.string().required('Required'),
+    zipCode: yup.string(),
+    phone: yup.string().required('Required'),
+    companyName: yup.string(),
+    orderNotes: yup.string(),
+   
+});
+export const CheckoutView = () => {
+    const [value, setValue] = useState('flatRate');
+    const [deliveryStatus, setDeliveryStatus] = useState('cashOnDelivery');
+    const [termsCondition, settermsCondition] = React.useState(false);
+    const [initialValues, setinInitialValues] = useState(billingDetails);
+    const [successAlert, setSuccessAlert] = useState(false);
+    const access_token = localStorage.getItem("accessToken");
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        enableReinitialize: true,
+        onSubmit: (values) => {
+                axios.post(`${process.env.REACT_APP_API_URI}order/`,
+                    values,
+                    {
+                        headers: {
+                            authorization: `${access_token}`,
+                        },
+                    }
+                )
+                    .then((res) => {
+                        if (res.data.success) {
+                            console.log(values)
+                            console.log(res.data)
+                            setSuccessAlert(true)
+                        }
+                    }).catch((error) => {
+                        console.log(error)
+                    })
+            
+        },
+
+    });
+    return (
+        <Container sx={{ padding: '40px 0' }}>
+            <form onSubmit={formik.handleSubmit}>
+                <Box sx={{padding: '6px 2px', margin: "8px 0", borderBottom: '1px dashed #2ec766'}}>
+                    <Typography sx={{fontWeight: '500', fontSize: '14px', color: '#8a8686'}}>Add your shipping details and procced your order.</Typography>
+                </Box>
+                <Grid container spacing={3}>
+                    <Grid item xs={8}>
+                        <BillingAdress formik={formik} />
+                    </Grid>
+                    <Grid item xs={4} >
+                        <FormControl sx={{ border: "2px solid #233a95", borderRadius: '7px', padding: "10px 8px" }}>
+                            <Typography variant='subtitle1' textAlign={'center'}>YOUR ORDER</Typography>
+                            <Divider />
+                            <TableContainer>
+                                <Table aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell sx={{ fontSize: 14, fontWeight: '600', color: '#71778e' }}>Product</TableCell>
+                                            <TableCell align="right" sx={{ fontSize: 14, fontWeight: '600', color: '#71778e' }}>Subtotal</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow
+                                        // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row" sx={{}}>
+                                                {"Angie's Boomchickapop Sweet & Salty Kettle Corn  × 1	"}
+                                            </TableCell>
+                                            <TableCell align="right" >{"$3.29"}</TableCell>
+                                        </TableRow>
+                                        <TableRow
+                                        // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row" sx={{}}>
+                                                {"SubTotal"}
+                                            </TableCell>
+                                            <TableCell align="right" >{"$3.29"}</TableCell>
+                                        </TableRow>
+                                        <TableRow
+                                        // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row" sx={{}}>
+                                                Shipping
+                                            </TableCell>
+                                            <TableCell align="left" >
+                                                <RadioGroup
+                                                    aria-label="gender"
+                                                    name="gender"
+                                                    value={value}
+                                                    onChange={(val) => setValue(val.target.value)}
+                                                >
+                                                    <FormControlLabel labelPlacement="start" value="flatRate" control={<Radio />} label={<Typography noWrap sx={{ display: 'inline-flex', fontSize: ".875rem", }}>Flat Rate <Typography style={{ color: '#d51243', fontSize: ".8125rem", marginLeft: '5px' }}> $5.00</Typography> </Typography>} />
+
+                                                    <FormControlLabel labelPlacement="start" value="localPickup" noWrap control={<Radio />}
+                                                        label={<Typography noWrap sx={{ fontSize: ".8125rem" }} >Local picup</Typography>}
+                                                    />
+                                                </RadioGroup>
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow
+                                        // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row" sx={{}}>
+                                                {"Total"}
+                                            </TableCell>
+                                            <TableCell align="right" >{"$8.29"}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <Box>
+                                <Box sx={{ padding: '6px 12px' }}>
+                                    <RadioGroup
+                                        aria-label="delivary status"
+                                        name="delivery"
+                                        value={deliveryStatus}
+                                        onChange={(val) => setDeliveryStatus(val.target.value)}
+                                    >
+                                        <FormControlLabel labelPlacement="end" value="cashOnDelivery" control={<Radio />} label={<Typography noWrap sx={{ color: '#233a95', fontSize: ".875rem", fontWeight: '600' }}>Cash on delivery</Typography>} />
+                                    </RadioGroup>
+                                </Box>
+
+                                <Divider />
+                                <Box sx={{ padding: '6px 12px', fontSize: '12px' }}>
+                                    <Typography sx={{ fontSize: '.8125rem', lineHeight: '1.6' }}>
+                                        Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our privacy policy.
+                                    </Typography>
+                                    <FormControlLabel control={<Checkbox checked={termsCondition}
+                                        onChange={(val) => settermsCondition(!termsCondition)} />} label={<Typography noWrap sx={{ display: 'inline-flex', fontSize: '.8125rem', }}>I have read and agree to the website  <Typography style={{ color: '#d51243', fontSize: '.8125rem', marginLeft: '5px' }}> terms and conditions </Typography> </Typography>} />
+                                </Box>
+                            </Box>
+                            <SubmitButton type="submit" disabled={!termsCondition} variant="contained">Place Order</SubmitButton>
+                        </FormControl>
+                    </Grid>
+
+                </Grid>
+            </form>
+        </Container>
+    )
+}
---
+++ src/pages/checkout/component/BillingAdress.jsx
@@ -0,0 +1,204 @@
+import styled from '@emotion/styled';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import { Box, Divider, Grid, TextField, Typography } from '@mui/material';
+import React, { useRef } from 'react';
+
+
+const billingDetails = {
+    email: "",
+    firstName: "",
+    lastName: "",
+    country: "",
+    houseandstreet: "",
+    apartment: "",
+    city: "",
+    zipCode: "",
+    phone: "",
+}
+export const BillingAdress = ({ formik }) => {
+    const dataFetchedRef = useRef(false);
+
+    // const formik = useFormik({
+    //     initialValues,
+    //     validationSchema,
+    //     enableReinitialize: true,
+    //     onSubmit: (values) => {
+    //         axios.patch(`${process.env.REACT_APP_API_URI}users/`,
+    //             values,
+    //             {
+    //                 headers: {
+    //                     authorization: `${access_token}`,
+    //                 },
+    //             }
+    //         )
+    //             .then((res) => {
+    //                 if (res.data.success) {
+    //                     setSuccessAlert(true)
+    //                 }
+    //             })
+    //     },
+    // });
+    return (
+        <Box sx={{ border: '1px solid gray', padding: '6px 10px', borderRadius: '6px' }}>
+            {/* <form onSubmit={formik.handleSubmit}> */}
+            <Box sx={{ padding: "6px 0" }}>
+                <Typography variant='subtitle1' sx={{ fontWeight: '600', padding: "6px 0" }}>BILLING DETAILS</Typography>
+                <Divider />
+            </Box>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <TextField
+                        label="First Name * "
+                        name="name.firstName"
+                        value={formik.values?.name?.firstName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.name?.firstName && Boolean(formik.errors.name?.firstName)}
+                        helperText={formik.touched.name?.firstName && formik.errors.name?.firstName}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        label="Last Name * "
+                        name="name.lastName"
+                        value={formik.values?.name?.lastName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.name?.lastName && Boolean(formik.errors.name?.lastName)}
+                        helperText={formik.touched.name?.lastName && formik.errors.name?.lastName}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} lg={12} md={12}>
+                    <TextField
+                        label="Company Name"
+                        name="comapnyName"
+                        value={formik.values?.comapnyName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.comapnyName && Boolean(formik.errors.comapnyName)}
+                        helperText={formik.touched.comapnyName && formik.errors.comapnyName}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} lg={12} md={12}>
+                    <TextField
+                        label="House No / Street Name"
+                        name="streetAddress"
+                        value={formik.values?.houseandstreet}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.houseandstreet && Boolean(formik.errors.houseandstreet)}
+                        helperText={formik.touched.houseandstreet && formik.errors.houseandstreet}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} lg={12} md={12}>
+                    <TextField
+                        label="Town / City"
+                        name="city"
+                        value={formik.values?.city}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.city && Boolean(formik.errors.city)}
+                        helperText={formik.touched.city && formik.errors.city}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} lg={12} md={12}>
+                    <TextField
+                        label="Zip Code"
+                        name="zipCode"
+                        value={formik.values?.zipCode}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.zipCode && Boolean(formik.errors.zipCode)}
+                        helperText={formik.touched.zipCode && formik.errors.zipCode}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6} lg={6} md={6}>
+                    <TextField
+                        label="Phone * "
+                        name="phone"
+                        value={formik.values?.phone}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.phone && Boolean(formik.errors.phone)}
+                        helperText={formik.touched.phone && formik.errors.phone}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6} lg={6} md={6}>
+                    <TextField
+                        label="Email"
+                        name="email"
+                        value={formik.values?.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} lg={12} md={12}>
+                    <Typography sx={{ fontSize: ".8125rem", padding: '6px' }}>Order notes (optional)</Typography>
+                    <StyledTextarea minRows={4} 
+                        aria-label="maximum height"
+                        placeholder="Maximum 4 rows"
+                        name="orderNotes"
+                        defaultValue="a gift from you your friend john"
+                        value={formik.values?.orderNotes}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.orderNotes && Boolean(formik.errors.orderNotes)}
+                        helperText={formik.touched.orderNotes && formik.errors.orderNotes}
+                    />
+                </Grid>
+
+            </Grid>
+            {/* </form> */}
+        </Box>
+    )
+}
+
+const StyledTextarea = styled(TextareaAutosize)(
+    ({ theme }) => `
+    width: 97%;
+    padding: 5px,
+    font-family: IBM Plex Sans, sans-serif;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 12px;
+    border-radius: 12px 12px 0 12px;
+    color: '#6e7781';
+    background: '#fff';
+    border: 1px solid  '#6e7781';
+    box-shadow: 0px 2px 2px '#d0d7de';
+    &:hover {
+      border-color: '#6e7781';
+    }
+
+    &:focus {
+      border-color: '#6e7781';
+    }
+
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `,
+);
+// const validationSchema = yup.object({
+//     email: yup.string().email('Invalid email').required('Required'),
+//     firstName: yup.string().required('Required'),
+//     lastName: yup.string().required('Required'),
+//     country: yup.string().required('Required'),
+//     houseandstreet: yup.string().required('Required'),
+//     apartment: yup.string().required('Required'),
+//     city: yup.string().required('Required'),
+//     zipCode: yup.string().required('Required'),
+//     phone: yup.string().required('Required'),
+// });
